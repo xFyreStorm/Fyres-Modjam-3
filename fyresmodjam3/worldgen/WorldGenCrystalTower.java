7e3c8362a9d645e05e9925db1d2510f1aae67fc9
Kenneth Dignam <kennethdignam@hotmail.com>
2013-12-14 11:32:17 -0500

    Working on tower gen.

--- fyresmodjam3/worldgen/WorldGenCrystalTower.java
+++ fyresmodjam3/worldgen/WorldGenCrystalTower.java
@@ -14,15 +14,16 @@ public class WorldGenCrystalTower implements IWorldGenerator {
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
 		if(world.provider.isSurfaceWorld()) {
 			if(chunkX == CommonTickHandler.worldData.overworldCrystalChunkX && chunkZ == CommonTickHandler.worldData.overworldCrystalChunkZ) {
-				for(int x = 4; x < 10; x++) {
-					for(int z = 4; z < 10; z++) {
+				for(int x = 0; x < 16; x++) {
+					for(int z = 0; z < 16; z++) {
 						for(int y = 0; y < 256; y++) {
-							if(z == 4 || z == 9 || x == 4 || x == 9 || y == 0 || y == 255) {
-								world.setBlock(chunkX * 16 + x, y, chunkZ * 16 + z, Block.obsidian.blockID);
-							} else if(x == 7 && z == 7 && (y == 1 || y == 2)) {
+							int dist = (x - 8) * (x - 8) + (z - 8) * (z - 8);
+							if(dist > 25 && dist <= 49 || (dist <= 49 && (y == 0 || y == 255))) {
+								world.setBlock(chunkX * 16 + x, y, chunkZ * 16 + z, (y == 255 && dist <= 25) ? Block.glass.blockID : (random.nextBoolean() ? Block.cobblestoneMossy.blockID : Block.cobblestone.blockID));
+							} else if(x == 8 && z == 8 && (y == 1 || y == 2)) {
 								if(y == 1) {world.setBlock(chunkX * 16 + x, y, chunkZ * 16 + z, FyresModjam3.crystalStand.blockID);}
 								else if(y == 2) {world.setBlock(chunkX * 16 + x, y, chunkZ * 16 + z, FyresModjam3.crystal.blockID);}
-							} else {
+							} else if(dist <= 25) {
 								world.setBlockToAir(chunkX * 16 + x, y, chunkZ * 16 + z);
 							}
 							
